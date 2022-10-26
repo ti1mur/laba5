67529e9dfeaa54997111db2dda2c1319faf3ef86
ti1mur <115170119+ti1mur@users.noreply.github.com>
2022-10-26 19:08:47 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,36 +2,29 @@
 
 using namespace std;
 
+int tr(int a, int b, int c) {
+	if ((a > 0) && (b > 0) && (c > 0)) {
+		if ((a < b + c) && (b < c + a) && (c < a + b)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return 0;
+	}
+}
 int main() {
-	setlocale(LC_ALL, "Rus");
-	int n, x, i_start, i_end, i_start_max, i_end_max, len, lenmax;
-	cout << "Введите желаемое кол-во элементов: ";
-	cin >> n;
-	int* massiv = new int[n];
-	cout << "Введите последовательность элементов: ";
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		massiv[i] = x;
-	}
-	i_start = 0;
-	len = 0;
-	lenmax = 0;
-	i_start_max = 0;
-	i_end_max = 0;
-	for (i_end = 0; i_end < n; i_end++) {
-		len = i_end - i_start + 1;
-		if (massiv[i_end] <= 0){
-			len = 0;
-			i_start = i_end + 1;
-		}
-		if (len > lenmax) {
-			lenmax = len;
-			i_start_max = i_start;
-			i_end_max = i_end + 1;
+	int m[10] = { 1, 2, 3, 4, -5, 6, 0, 7, -1000, 10 };
+	for (int i = 0; i < 8; i++) {
+		for (int y = i + 1; y < 9; y++) {
+			for (int x = y + 1; x < 10; x++) {
+				if (tr(m[i], m[y], m[x]) == 1) {
+					cout << m[i] << "  " << m[y] << "  " << m[x] << endl;
+				}
+			}
 		}
 	}
-	for (int shet = i_start_max; shet < i_end_max; shet++) {
-		cout << massiv[shet] << " ";
-	}
-	delete[] massiv;
+	return 0;
 }
