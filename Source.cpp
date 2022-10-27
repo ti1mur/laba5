99ef8654caf35ad69aa2c68dfb5c6d514a3274d8
ti1mur <115170119+ti1mur@users.noreply.github.com>
2022-10-27 18:12:05 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int tr(int a, int b, int c) {
+int triangle(int a, int b, int c) {
 	if ((a > 0) && (b > 0) && (c > 0)) {
 		if ((a < b + c) && (b < c + a) && (c < a + b)) {
 			return 1;
@@ -16,12 +16,12 @@ int tr(int a, int b, int c) {
 	}
 }
 int main() {
-	int m[10] = { 1, 2, 3, 4, -5, 6, 0, 7, -1000, 10 };
-	for (int i = 0; i < 8; i++) {
-		for (int y = i + 1; y < 9; y++) {
-			for (int x = y + 1; x < 10; x++) {
-				if (tr(m[i], m[y], m[x]) == 1) {
-					cout << m[i] << "  " << m[y] << "  " << m[x] << endl;
+	int m[10] = { 1, 2, 3, 4, -5, 6, 0, 7, -1000, 10};
+	for (int shet1 = 0; shet1 < sizeof(m)/4 - 2; shet1++) {
+		for (int shet2 = shet1 + 1; shet2 < sizeof(m) / 4 - 1; shet2++) {
+			for (int shet3 = shet2 + 1; shet3 < sizeof(m) / 4 ; shet3++) {
+				if (triangle(m[shet1], m[shet2], m[shet3]) == 1) {
+					cout << m[shet1] << "  " << m[shet2] << "  " << m[shet3] << endl;
 				}
 			}
 		}
